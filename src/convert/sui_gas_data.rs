ac634cd09f58f5436ffe6007fea164ea51128bdb
ppoliani <ppoliani@gmail.com>
2023-05-16 21:30:39 +0300

    feat: implement convert_sui_gas_data

--- src/convert/sui_gas_data.rs
+++ src/convert/sui_gas_data.rs
@@ -1,11 +1,12 @@
 use sui_json_rpc_types::SuiGasData;
 use crate::pb::sui::checkpoint as pb;
+use super::common::convert_sui_object_ref;
 
 pub fn convert_sui_gas_data(source: &SuiGasData) -> pb::SuiGasData {
   pb::SuiGasData {
     payment: source.payment.iter().map(convert_sui_object_ref).collect(),
-    owner: todo!(),
-    price: todo!(),
-    budget: todo!(),
+    owner: source.owner.to_vec(),
+    price: source.price,
+    budget: source.budget,
   }
 }
